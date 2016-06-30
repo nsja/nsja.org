d266bedee23488a009099575a454f7a60d43dc0a
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-06-30 10:43:36 -0400

    Fixed form signup

--- scripts/js/chimp.js
+++ scripts/js/chimp.js
@@ -1,30 +1,38 @@
-$(document).ready( function () {
+$(document).ready(function() {
     var $form = $('form');
 
     if ($form.length > 0) {
-        $('form input[type="submit"]').bind('click', function (event) {
+        $('form input[type="submit"]').on('click', function(event) {
             if (event) event.preventDefault();
 
             $.ajax({
                 type: $form.attr('method'),
                 url: $form.attr('action'),
                 data: $form.serialize(),
-                cache       : false,
-                dataType    : 'json',
-                contentType : "application/json; charset=utf-8",
-                error       : function(err) {
-                  $('.message').css('display','block');
-                  $('.message').html("Could not connect to the registration server. Please try again later.");
-                  $('.message').delay(3000).fadeOut('slow');
+                cache: false,
+                dataType: 'jsonp',
+                jsonp: 'c',
+                contentType: 'application/jsonp; charset=utf-8',
+                error: function(err) {
+                    $('.message').css('display', 'block');
+                    $('.message').html("Could not connect to the registration server. Please try again later.");
+                    $('.message').delay(3000).fadeOut('slow');
                 },
-                success     : function(data) {
-                  $('.message').css('display','block');
+                success: function(data) {
+                    $('.message').css('display', 'block');
 
                     if (data.result != "success") {
-                        var result = data.msg;
-                        $('.message').html(result);
+                        var result = data.msg.split(' - ');
+                        var display = result[1];
+
+                        if (display[display.length-1] !== '.') {
+                            display += '.';
+                        }
+
+                        $('.message').html(display);
                     } else {
                         $('.message').html('You have been subscribed. Please check your email to confirm your subscription.');
+                        $('input[type="text"], input[type="email"]').val('');
                     }
 
                     $('.message').delay(3000).fadeOut('slow');
